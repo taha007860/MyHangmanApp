25d95a50c52c250c66247eca07c78e7952550c74
Mohammad Zaatari <mohamadalzaatari748@gmail.com>
2023-01-22 22:39:19 +0200

    added reset button

--- hangman/src/components/Algorithm.js
+++ hangman/src/components/Algorithm.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 const words = ['javascript', 'python', 'java', 'c++', 'c#'];
-
 const Algorithm = () => {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
-  const maxGuesses = 6;
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [misses, setMisses] = useState([]);
-  const [displayWord, setDisplayWord] = useState(randomWord.replace(/[a-zA-Z]/g, "_"));
+  const [displayWord, setDisplayWord] = useState("");
+  const [randomWord, setRandomWord] = useState("");
+  const maxGuesses = 6;
+  useEffect(() => {
+    setRandomWord(words[Math.floor(Math.random() * words.length)])
+    setDisplayWord(randomWord.replace(/[a-zA-Z]/g, "_"))
+  }, [])
 
   const handleGuess = (e) => {
     e.preventDefault();
     const letter = e.target.elements.letter.value;
     // check if the letter is already guessed
-    if (guessedLetters.includes(letter)) {
-      alert(`You already guessed "${letter}", try another letter`);
+    if (guessedLetters.includes(letter) || !/^[a-zA-Z]+$/.test(letter)) {
+      alert(`You already guessed "${letter}" or entered a wrong input, try another letter`);
       return;
     }
     // add the letter to the guessedLetters array
@@ -30,11 +32,18 @@ const Algorithm = () => {
     e.target.elements.letter.value = "";
   };
 
+  const handleReset = () => {
+    setGuessedLetters([]);
+    setMisses([]);
+    setRandomWord(words[Math.floor(Math.random() * words.length)])
+    setDisplayWord(randomWord.replace(/[a-zA-Z]/g, "_"));
+  };
+
   if (misses.length === maxGuesses) {
-    return <div>You Lost! The word was: {randomWord}</div>;
+    return <div>You Lost! The word was: {randomWord} <button onClick={handleReset}>Play Again</button> </div>;
   }
   if (displayWord === randomWord) {
-    return <div>You Won! The word was: {randomWord}</div>;
+    return <div>You Won! The word was: {randomWord} <button onClick={handleReset}>Play Again</button> </div>;
   }
 
   return (
@@ -47,8 +56,9 @@ const Algorithm = () => {
           <input type="text" name="letter" maxLength={1} />
         </label>
         <button type="submit">Guess</button>
+        <button type="button" onClick={handleReset}>Reset</button>
       </form>
     </div>
   );
-  }
-  export default Algorithm;
+}
+export default Algorithm;
